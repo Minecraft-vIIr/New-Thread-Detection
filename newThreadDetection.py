d6563a5b7caad40c189bc8c6064367150020c2bd
Minecraft-vIIr <97388426+Minecraft-vIIr@users.noreply.github.com>
2023-11-29 13:23:28 +0800

    Update newThreadDetection.py

--- newThreadDetection.py
+++ newThreadDetection.py
@@ -24,7 +24,16 @@ watcher = c.watch_for(raw_wql=raw_wql)
 while True:
     process_created = watcher()
 
-    print(f'\033[1;0m{time.strftime("[%Y.%m.%d %H:%M:%S]")}\nProcess Name: \033[1;32m{getattr(process_created, "Name", None)}\n\033[0mParent PID: \033[1;32m{getattr(process_created, "ParentProcessId", None)}\n\033[0mPID: \033[1;32m{getattr(process_created, "ProcessId", None)}\n\033[0mCommand Line: \033[1;32m{getattr(process_created, "CommandLine", None)}\n\n')
+    print(f'\033[1;0m{time.strftime("[%Y.%m.%d %H:%M:%S]")}')
+    print(f'\033[0mProcess Name: \033[1;32m{getattr(process_created, "Name", None)}')
+    print(f'\033[0mParent PID: \033[1;32m{getattr(process_created, "ParentProcessId", None)}')
+    print(f'\033[0mPID: \033[1;32m{getattr(process_created, "ProcessId", None)}')
+    print(f'\033[0mCommand Line: \033[1;32m{getattr(process_created, "CommandLine", None)}')
+    print()
 
     with open(os.path.join(save_path, f'{time.strftime("%Y%m%d-%H%M")}.txt'), "a", encoding="utf-8") as f:
-        f.write(f'{time.strftime("[%Y.%m.%d %H:%M:%S]")}\nProcess Name: {getattr(process_created, "Name", None)}\nParent PID: {getattr(process_created, "ParentProcessId", None)}\nPID: {getattr(process_created, "ProcessId", None)}\nCommand Line: {getattr(process_created, "CommandLine", None)}\n\n')
+        f.write(f'{time.strftime("[%Y.%m.%d %H:%M:%S]")}\n')
+        f.write(f'Process Name: {getattr(process_created, "Name", None)}\n')
+        f.write(f'Parent PID: {getattr(process_created, "ParentProcessId", None)}\n')
+        f.write(f'PID: {getattr(process_created, "ProcessId", None)}\n')
+        f.write(f'Command Line: {getattr(process_created, "CommandLine", None)}\n\n')
